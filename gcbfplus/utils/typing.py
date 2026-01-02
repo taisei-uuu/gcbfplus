7bdc8d6d8b0d22598f3fb4ab9c82fcfe1278e3c7
Taisei <taisei040428@gmail.com>
2026-01-02 12:02:05 +0900

    jaxtyping dependency error

--- gcbfplus/utils/typing.py
+++ gcbfplus/utils/typing.py
@@ -1,39 +1,42 @@
 from flax import core, struct
-from jaxtyping import Array, Bool, Float, Int, Shaped
+# from jaxtyping import Array, Bool, Float, Int, Shaped
+from typing import Dict, TypeVar, Any, List, Union
+from jax import Array
+import jax.numpy as jnp
 from typing import Dict, TypeVar, Any, List
 from numpy import ndarray
 
 
 # jax types
-PRNGKey = Float[Array, '2']
+PRNGKey = Any
 
-BoolScalar = Bool[Array, ""]
-ABool = Bool[Array, "num_agents"]
+BoolScalar = Any
+ABool = Any
 
 # environment types
-Action = Float[Array, 'num_agents action_dim']
-Reward = Float[Array, '']
-Cost = Float[Array, '']
+Action = Any
+Reward = Any
+Cost = Any
 Done = BoolScalar
-Info = Dict[str, Shaped[Array, '']]
-EdgeIndex = Float[Array, '2 n_edge']
-AgentState = Float[Array, 'num_agents agent_state_dim']
-State = Float[Array, 'num_states state_dim']
-Node = Float[Array, 'num_nodes node_dim']
-EdgeAttr = Float[Array, 'num_edges edge_dim']
-Pos2d = Float[Array, '2'] | Float[ndarray, '2']
-Pos3d = Float[Array, '3'] | Float[ndarray, '3']
-Pos = Pos2d | Pos3d
-Radius = Float[Array, ''] | float
+Info = Dict[str, Any]
+EdgeIndex = Any
+AgentState = Any
+State = Any
+Node = Any
+EdgeAttr = Any
+Pos2d = Union[Array, ndarray]
+Pos3d = Union[Array, ndarray]
+Pos = Union[Pos2d, Pos3d]
+Radius = Union[Array, float]
 
 
 # neural network types
 Params = TypeVar("Params", bound=core.FrozenDict[str, Any])
 
 # obstacles
-ObsType = Int[Array, '']
-ObsWidth = Float[Array, '']
-ObsHeight = Float[Array, '']
-ObsLength = Float[Array, '']
-ObsTheta = Float[Array, '']
-ObsQuaternion = Float[Array, '4']
+ObsType = Any
+ObsWidth = Any
+ObsHeight = Any
+ObsLength = Any
+ObsTheta = Any
+ObsQuaternion = Any
